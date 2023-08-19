8ee019e94056f76bba86e3915b94ca1791f8ae6f
felipereyel <felipe.reyel@gmail.com>
2023-08-18 23:22:22 -0300

    Migrations!

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
+	"fintracker/pkgs/config"
 	"fintracker/pkgs/handlers"
+	_ "fintracker/pkgs/migrations"
 	"log"
 	"os"
 
-	_ "github.com/joho/godotenv/autoload"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
 	app := pocketbase.New()
-	publicDir := os.Getenv("PUBLIC_DIR")
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: config.Envs.Automigrate,
+	})
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/healthz", handlers.Healthz)
-		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS(publicDir), true))
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS(config.Envs.PublicDir), true))
 		return nil
 	})
 
